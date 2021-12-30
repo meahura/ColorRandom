b3fc5eab2de836e96ad99f6c5981273ba3bf57f3
Ahoora <75671611+AhSiber@users.noreply.github.com>
2021-12-30 19:08:10 +0330

    Update colorMeakUP.py

--- color_random_txt/colorMeakUP.py
+++ color_random_txt/colorMeakUP.py
@@ -1,4 +1,6 @@
 import random
+import sys
+
 class color : 
     def __init__(self) -> None:pass
     def Color(text:str): 
@@ -23,3 +25,31 @@ class color :
         elif random_ == 14:print(f"\033[93m{Dim}{text}") 
         elif random_ == 15:print(f"\033[94m{Underlined}{text}")   
         else:raise ValueError(f'\033[1mError value in text')
+        # list color in pyhton ! 
+
+# sys argv - input 
+try : 
+    ext = sys.argv[1:100]
+    text =  " ".join(ext) 
+    ResetAll = "\033[0m";Bold       = "\033[1m";Dim        = "\033[2m"
+    Underlined = "\033[4m";Blink      = "\033[5m";Reverse    = "\033[7m";Hidden     = "\033[8m" 
+    # 'random color : text def color 16 color ' 
+    random_ = random.randrange(1,16)
+    if random_ == 1 :print(f"\033[39m{ResetAll}{text}")
+    elif random_ == 2:print(f"\033[30m{Bold}{text}") 
+    elif random_ == 3:print(f"\033[31m{Dim}{text}") 
+    elif random_ == 4:print(f"\033[32m{Underlined}{text}") 
+    elif random_ == 5:print(f"\033[33m{Blink}{text}") 
+    elif random_ == 6:print(f"\033[34m{Reverse}{text}") 
+    elif random_ == 7:print(f"\033[35m{ResetAll}{text}") 
+    elif random_ == 8:print(f"\033[35m{Bold}{text}") 
+    elif random_ == 9:print(f"\033[36m{Dim}{text}") 
+    elif random_ == 10:print(f"\033[37m{Underlined}{text}") 
+    elif random_ == 11:print(f"\033[90m{Blink}{text}") 
+    elif random_ == 12:print(f"\033[91m{Reverse}{text}") 
+    elif random_ == 13:print(f"\033[92m{ResetAll}{text}") 
+    elif random_ == 14:print(f"\033[93m{Dim}{text}") 
+    elif random_ == 15:print(f"\033[94m{Underlined}{text}")   
+    else:raise ValueError(f'\033[1mError value in text')
+except: 
+    pass  
