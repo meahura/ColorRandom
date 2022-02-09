9d6671390abe1614aaa02cca09776b5d6fed223f
Ahoora <75671611+AhSiber@users.noreply.github.com>
2022-02-09 19:51:56 +0330

    Update colorMeakUP.py

--- color_random_txt/colorMeakUP.py
+++ color_random_txt/colorMeakUP.py
@@ -35,7 +35,25 @@ try :
     Underlined = "\033[4m";Blink      = "\033[5m";Reverse    = "\033[7m";Hidden     = "\033[8m" 
     # 'random color : text def color 16 color ' 
     random_ = random.randrange(1,16)
-    if random_ == 1 :print(f"\033[39m{ResetAll}{text}")
+    
+    if text == "-h" or text =="--help":
+        print('''
+[name File] [text] 
+
+for example : colorMeakUP.py [text]
+For Example to python code : color.Color() [Enter Text(String)]
+
+OPTIONS
+    -h --helo                           helo tools colorRandom
+    -o --owner                          owner colorRandom''')
+    elif text == "-o" or text == "--owner": 
+        print('''\033[31m
+            github : http://github.com/AhSiber 
+            Telegram : https://t.me/SI_Developers
+            virgool : http://virgool.io/@ahura 
+''')
+
+    elif random_ == 1 :print(f"\033[39m{ResetAll}{text}")
     elif random_ == 2:print(f"\033[30m{Bold}{text}") 
     elif random_ == 3:print(f"\033[31m{Dim}{text}") 
     elif random_ == 4:print(f"\033[32m{Underlined}{text}") 
@@ -49,7 +67,7 @@ try :
     elif random_ == 12:print(f"\033[91m{Reverse}{text}") 
     elif random_ == 13:print(f"\033[92m{ResetAll}{text}") 
     elif random_ == 14:print(f"\033[93m{Dim}{text}") 
-    elif random_ == 15:print(f"\033[94m{Underlined}{text}")   
+    elif random_ == 15:print(f"\033[94m{Underlined}{text}")
     else:raise ValueError(f'\033[1mError value in text')
 except: 
-    pass  
+    pass
