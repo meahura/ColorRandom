c8c312cb27fc56954ca90037d8fce1aa110edfc8
Ahoora <75671611+AhSiber@users.noreply.github.com>
2022-02-27 19:05:09 +0330

    Update colorMeakUP.py

--- color_random_txt/colorMeakUP.py
+++ color_random_txt/colorMeakUP.py
@@ -1,5 +1,6 @@
 import random
 import sys
+from os import system 
 
 class color : 
     def __init__(self) -> None:pass
@@ -44,27 +45,42 @@ try :
     Underlined = "\033[4m";Blink      = "\033[5m";Reverse    = "\033[7m";Hidden     = "\033[8m" 
     # 'random color : text def color 16 color ' 
             # colors
-    Cyan = "\033[36m"  ; Purple = "\033[35m" ; blue = "\033[34" 
+    Cyan = "\033[36m"  ; Purple = "\033[35m" ; blue = "\033[34m" 
     Brown = "\033[33m" ; Dark_Gray = "\033[1;30m"
 
     random_ = random.randrange(1,21)
     
     if text == "-h" or text =="--help":
-        print('''
-[name File] [text] 
+        system('clear')
+        print(f'''
+{Dark_Gray}name File] [text] 
 
 for example : colorMeakUP.py [text]
 For Example to python code : color.Color() [Enter Text(String)]
 
 OPTIONS
     -h --helo                           helo tools colorRandom
-    -o --owner                          owner colorRandom''')
+    -o --owner                          owner colorRandom
+    -c --color                          show tables color''')
     elif text == "-o" or text == "--owner": 
         print('''\033[31m
             github : http://github.com/AhSiber 
             Telegram : https://t.me/SI_Developers
             virgool : http://virgool.io/@ahura 
 ''')
+    elif text == "-c" or text =="--color":
+        system('clear')
+        print(f"""
+The colors that this tool supports : \033[39m█\033[30m█\033[31m█\033[32m█\033[33m█\033[34m█\033[35m█\033[36m█\033[37m█\033[90m█\033[92m█\033[93m█\033[94m█{Cyan}█{Purple}█{Brown}█{Dark_Gray}█{blue}█\033[95m█\033[96m█\033[97m█\033[98m█\033[99m█\033[100m█
+{ResetAll}Changes to text : {Bold}bold ~ {Underlined}Underlined ~ {ResetAll}ResetAll ~ {Blink}Blink ~ {Dim}Dim ~ {Reverse}Reverse
+
+{ResetAll}{Bold}
+NEWS:
+(1) - All colors will be supported soon
+(2) - New and lighter features
+(3) - With other languages such as (JavaScript , Ruby...) It's written.
+""")
+
 
     elif random_ == 1 :print(f"\033[39m{ResetAll}{text}")
     elif random_ == 2:print(f"\033[30m{Bold}{text}") 
