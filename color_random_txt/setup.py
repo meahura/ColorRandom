8930120e960986eb5c4ceff5a53214ad2c19943f
Ahoora <75671611+AhSiber@users.noreply.github.com>
2022-05-07 10:06:33 +0430

    Create setup.py

---
+++ color_random_txt/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup 
+
+
+setup(
+   name='ColorRandom',
+   version='1.1.10', 
+   description="It's a cool tool for generating random colors in pythons that have a lot of colors.",
+   long_description='en', 
+   url='https://github.com/AhSiber/ColorRandom', 
+   scripts=['colorMeakUP.py'], 
+   command_packages=['-h','-o','c']
+)
