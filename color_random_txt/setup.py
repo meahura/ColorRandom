c613f3f3d3ec148cafbce47170bc3566285e613d
AhSiber <Ahurarasoli@gmail.com>
2022-05-17 22:36:18 +0430

    space

--- color_random_txt/setup.py
+++ color_random_txt/setup.py
@@ -3,10 +3,10 @@ from distutils.core import setup
 
 setup(
    name='ColorRandom',
-   version='v1.21', 
+   version='v1.22', 
    description="It's a cool tool for generating random colors in pythons that have a lot of colors.",
    long_description='en', 
    url='https://github.com/AhSiber/ColorRandom', 
-   scripts=['colorMeakUP.py'], 
+   scripts=['Main.file.py'], 
    command_packages=['-h','-o','c']
 )
