bf2121f893c4870c5a2d3e2f8beec2f9ec58388a
AhSiber <Ahurarasoli@gmail.com>
2022-05-25 22:27:13 +0430

    setup project!

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup 
+
+setup(
+   name='ColorRandom', 
+   version='v3.0.1',
+   description='random color to txt',
+   url='https://github.com/AhSiber/ColorRandom',
+   download_url='https://github.com/AhSiber/ColorRandom.git',
+   script_name='ColorRandom'
+)
